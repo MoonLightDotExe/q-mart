5e59c55ea91cc29168fbc691c2050b8969633e9f
Nilesh119 <117853068+Nilesh119@users.noreply.github.com>
2023-05-31 01:12:55 +0530

    navber and footer 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer/Footer'
+import  { useEffect, useState } from 'react';
 
 const App = () => {
+  const [scrolled, setScrolled] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div>
-        <>
+    <div className='app-container'>
+      <header className={`header ${scrolled ? 'scrolled' : ''}`}>
        <Navbar/>
-      </>
+
+      </header>
+        <footer>
+       <Footer/>
+      </footer>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './Navbar.css';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom'
+import {AiOutlineUser } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';  
+import { AiOutlineShopping  } from "react-icons/ai";
+import {AiOutlineBars} from "react-icons/ai";
 function Navbar() {
   return (
-    <div className='Nav'>
-        nilesh
-      
+    <div className="container">
+      <div className="contact">contact us</div>
+        <div className='Nav'>Q-Mart</div>
+      <ul className='menu'>
+        <li>
+          <a href="/">
+            <AiOutlineUser
+             size = {24}
+             style = {{color: 'black'}}
+             /> 
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <AiOutlineShopping 
+            size = {24}
+            style={{ color: 'black' }}/> 
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <BiSearch
+            size = {24}
+            style={{ color: 'black' }}/> 
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <AiOutlineBars
+            size = {24}
+            style={{ color: 'black' }}/> 
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
