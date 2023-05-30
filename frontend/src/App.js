bdd61543d1a43dafed5e5fc80eb90ebda2c6b586
Gautami <guptagautami-cmpn@atharvacoe.ac.in>
2023-05-30 23:25:08 +0530

    login almost done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+// import React from 'react'
+import React, { useRef } from 'react';
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <div className='login-page'>
+      <Router>
+        <Routes>
+          <Route path="/login" Component={Login} element={<Login />} />
+          <Route path="/register" Component={Login} element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
+
+// npm run client when in q-mart 
