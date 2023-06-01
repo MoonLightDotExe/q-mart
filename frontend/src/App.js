aecc484fe5abc9f7697fb33766cc0fea23ac541d
Nilesh119 <117853068+Nilesh119@users.noreply.github.com>
2023-06-02 01:33:09 +0530

    create item selector

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
-
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer/Footer'
+import Item from './components/Item/Item'
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return <div >
+    <>
+    <Navbar/>
+    <Item/>
+    <Footer/>
+    </>
+  </div>
 }
 
 export default App
---
+++ frontend/src/components/Item/Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Item.css'
+
+const ItemSelector = () => {
+  return (
+    <div className='Item-Selector'>
+        <div className="title">
+            VARIATIONS
+            <span dir='ltr'>(13)</span>
+        </div>
+        <div className="Item-Selector-Image">
+            
+        </div>
+    </div>
+  )
+}
+
+export default ItemSelector
+
+
+
+
+
